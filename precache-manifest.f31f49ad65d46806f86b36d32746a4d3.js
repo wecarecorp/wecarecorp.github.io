35ca798c855942b8f6b7d011dce8c935e3dc88b2
Rafael Fernandes <spnrafael@gmail.com>
2020-01-17 19:20:44 -0300

    Updates

---
+++ precache-manifest.f31f49ad65d46806f86b36d32746a4d3.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88ad8f084d6a520dfe42ec31202eb4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4bd8b6cedff8f642e64",
+    "url": "/static/css/2.0f35903b.chunk.css"
+  },
+  {
+    "revision": "0265bb553be9867975ad",
+    "url": "/static/css/main.fbfeb547.chunk.css"
+  },
+  {
+    "revision": "c4bd8b6cedff8f642e64",
+    "url": "/static/js/2.69c13b54.chunk.js"
+  },
+  {
+    "revision": "18929c19960f1c507fd6f7966f645ec3",
+    "url": "/static/js/2.69c13b54.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0265bb553be9867975ad",
+    "url": "/static/js/main.4860ac4e.chunk.js"
+  },
+  {
+    "revision": "8cd70c187cf1aab1aa28",
+    "url": "/static/js/runtime-main.a0d38efc.js"
+  },
+  {
+    "revision": "2ed3425f0170108e177593cc4f2e73eb",
+    "url": "/static/media/Gilroy-Black.2ed3425f.ttf"
+  },
+  {
+    "revision": "b381c2abd2972024a6a7e3d882208d9b",
+    "url": "/static/media/Gilroy-Bold.b381c2ab.ttf"
+  },
+  {
+    "revision": "4b236c6cb4c59d66b80dde6f9c614ebd",
+    "url": "/static/media/Gilroy-Light.4b236c6c.ttf"
+  },
+  {
+    "revision": "c83281ae1ca703d0741a770ee7e7c091",
+    "url": "/static/media/Gilroy-Medium.c83281ae.ttf"
+  },
+  {
+    "revision": "31ff7c1a62a300dbbf9656b4ba14a0d5",
+    "url": "/static/media/Gilroy-Regular.31ff7c1a.ttf"
+  },
+  {
+    "revision": "a5cf732b15078843b237bd58f3ed44cd",
+    "url": "/static/media/Gilroy-SemiBold.a5cf732b.ttf"
+  },
+  {
+    "revision": "1d8e75d9185a1e4d2204cc88b072782a",
+    "url": "/static/media/caixa.1d8e75d9.png"
+  },
+  {
+    "revision": "6ee2fce2e99a982eb611e35511affc95",
+    "url": "/static/media/campaign-1.6ee2fce2.svg"
+  },
+  {
+    "revision": "0d62d15b1d4055dd7146fbb79fcbd743",
+    "url": "/static/media/campaign-2.0d62d15b.svg"
+  },
+  {
+    "revision": "df55d5ec2b54ce0065c4e0b9d771ebbe",
+    "url": "/static/media/campaign-3.df55d5ec.svg"
+  },
+  {
+    "revision": "a1d88147c899b8f67532a16adc3f74c0",
+    "url": "/static/media/campaign-4.a1d88147.svg"
+  },
+  {
+    "revision": "8f1719d023d72f369e36e282a5b0899c",
+    "url": "/static/media/campaign-5.8f1719d0.svg"
+  },
+  {
+    "revision": "f4e1d738acbc93953903cc0491e1c4fc",
+    "url": "/static/media/campaign-6.f4e1d738.svg"
+  },
+  {
+    "revision": "8315bb68576a2521586ff978dcd03516",
+    "url": "/static/media/email.8315bb68.svg"
+  },
+  {
+    "revision": "7e8d42ae165165abce2f045680ee0626",
+    "url": "/static/media/facebook-brands.7e8d42ae.svg"
+  },
+  {
+    "revision": "bc445415cec026d33b72464fd6e4d977",
+    "url": "/static/media/ilustracao-banner.bc445415.svg"
+  },
+  {
+    "revision": "088e7b0cbeda10ec203311c9c6a13535",
+    "url": "/static/media/instagram-brands.088e7b0c.svg"
+  },
+  {
+    "revision": "989f62bce3de6a07de0840d3e5c65fe5",
+    "url": "/static/media/linkedin-brands.989f62bc.svg"
+  },
+  {
+    "revision": "ddd714f647a1d49417567709e499059c",
+    "url": "/static/media/logo-wecare-gray.ddd714f6.svg"
+  },
+  {
+    "revision": "ecbb98736a6885b6262873f237d8cdb8",
+    "url": "/static/media/logo-wecare.ecbb9873.svg"
+  },
+  {
+    "revision": "d13f3f94e8495a34c0fdbf5ef51c0151",
+    "url": "/static/media/map-marker.d13f3f94.svg"
+  },
+  {
+    "revision": "c8113a6d182040f973e70f1598aa9606",
+    "url": "/static/media/phone.c8113a6d.svg"
+  },
+  {
+    "revision": "b98d20a0b88c06ae90396127258e9258",
+    "url": "/static/media/plataforma.b98d20a0.png"
+  },
+  {
+    "revision": "1484c362d89403830674f572636c9a6e",
+    "url": "/static/media/problem-bottom.1484c362.svg"
+  },
+  {
+    "revision": "17bcf4b1329a435226032e5e9bd206c8",
+    "url": "/static/media/problem-top.17bcf4b1.svg"
+  },
+  {
+    "revision": "93d2e9137177c070976ab93dc2856756",
+    "url": "/static/media/problem-topic-icon.93d2e913.svg"
+  },
+  {
+    "revision": "cb09d418662f9d0d862cbbbe84dea11b",
+    "url": "/static/media/step-1.cb09d418.svg"
+  },
+  {
+    "revision": "44ac6dbec49e1bc7c4a6f3d590704baf",
+    "url": "/static/media/step-2.44ac6dbe.svg"
+  },
+  {
+    "revision": "628679d2386be3ed212b7ac1d56885e0",
+    "url": "/static/media/step-3.628679d2.svg"
+  },
+  {
+    "revision": "5cb7ded69b0a0c266f97d52ac53aa4e1",
+    "url": "/static/media/step-4.5cb7ded6.svg"
+  },
+  {
+    "revision": "04e183144eff4b93f5aab0eb77f230e4",
+    "url": "/static/media/subheader-bg.04e18314.svg"
+  },
+  {
+    "revision": "3699f91b4bea6ffb77b9b12a8d0fe22f",
+    "url": "/static/media/why-bottom.3699f91b.svg"
+  },
+  {
+    "revision": "17bcf4b1329a435226032e5e9bd206c8",
+    "url": "/static/media/why-top.17bcf4b1.svg"
+  },
+  {
+    "revision": "00407fd45029d7de6131a191a0fde539",
+    "url": "/static/media/why-wave.00407fd4.svg"
+  }
+]);
--- src/pages/Demo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Demo = () => {
-  return (
-    <h1>Demo</h1>
-  )
-}
-
-export default Demo;
